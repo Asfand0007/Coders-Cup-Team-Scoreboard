3dfae88ff191011a678e4e42861cf45921c01162
Sarim Ahmed <C41f0N@protonmail.com>
2024-11-17 20:02:26 +0500

    credits adjusted accrording to new position

--- frontend/src/components/Credits.js
+++ frontend/src/components/Credits.js
@@ -6,7 +6,7 @@ export default function Credits() {
             <div className="rounded-full text-xs border-[#f7b72e] border-2 font-bold bg-black/5 backdrop-blur-md py-1 px-2.5 text-[#f7b72e] shadow-sm">
                 ?
             </div>
-            <span className="vsm:w-56 w-48 absolute bottom-10 right-5 scale-0 rounded-lg border-4 border-black/10 bg-black/10 p-4 vsm:text-s text-xs text-[#d6d6d6] group-hover:scale-100 backdrop-blur-md shadow-sm">
+            <span className="vsm:w-56 w-48 absolute bottom-10 left-5 scale-0 rounded-lg border-4 border-black/10 bg-black/10 p-4 vsm:text-s text-xs text-[#d6d6d6] group-hover:scale-100 backdrop-blur-md shadow-sm">
                 <p className="font-bold vsm:text-lg text-base text-[#f7b72e] mb-0.5">
                     Developed By
                 </p>
